a6dd031c46d0ba161c17ebbcda5bd27db208bb43
Eduardo <eduardo@rushers.org>
2015-10-28 21:34:58 -0200

    Fix problem with import name

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, unicode_literals
 
 from django.conf.urls import url, include
 from rest_framework import routers
-from rest_framework.authtoken import views
+import rest_framework.authtoken as drf_token
 
 from . import views
 
@@ -12,7 +12,7 @@ router.register(r'stops', views.StopViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-token-auth/', views.obtain_auth_token),
+    url(r'^api-token-auth/', drf_token.views.obtain_auth_token),
     url(r'^osm_import/$', views.osm_import),
 ]
 
