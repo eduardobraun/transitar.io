99a2d0ce732f5a148902dcb834af40f5609eab4c
Eduardo <eduardo@rushers.org>
2015-10-28 21:24:08 -0200

    Add DRF token-auth view

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, unicode_literals
 
 from django.conf.urls import url, include
 from rest_framework import routers
+from rest_framework.authtoken import views
 
 from . import views
 
@@ -11,8 +12,7 @@ router.register(r'stops', views.StopViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^auth/', include('rest_framework.urls',
-                                       namespace='rest_framework')),
+    url(r'^api-token-auth/', views.obtain_auth_token),
     url(r'^osm_import/$', views.osm_import),
 ]
 
